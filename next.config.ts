2370abba7eba610dc212627628b68dbb6657b49a
Ashikur Rahman Bhuiyan <ashikurrb@outlook.com>
2026-04-26 13:48:31 +0600

    use r2 fall back for video

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-2593b1c3b7a14e21989c27d38f3a4612.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
